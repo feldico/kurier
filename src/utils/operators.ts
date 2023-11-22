308a0550eeb0680e167f287c8063d24ae48c659c
Fernando <fernando@pop-os.localdomain>
2023-11-22 11:40:09 -0300

    Se agregó operator ilike

--- src/utils/operators.ts
+++ src/utils/operators.ts
@@ -1,17 +1,18 @@
-export type OperatorName = "eq" | "ne" | "lt" | "gt" | "le" | "ge" | "like" | "nlike" | "in" | "nin";
+export type OperatorName = 'eq' | 'ne' | 'lt' | 'gt' | 'le' | 'ge' | 'like' | 'ilike' | 'nlike' | 'in' | 'nin'
 
 export const KnexOperators = {
-  eq: "=",
-  ne: "!=",
-  lt: "<",
-  gt: ">",
-  le: "<=",
-  ge: ">=",
-  like: "like",
-  nlike: "not like",
-  in: "in",
-  nin: "not in",
-};
+  eq: '=',
+  ne: '!=',
+  lt: '<',
+  gt: '>',
+  le: '<=',
+  ge: '>=',
+  like: 'like',
+  ilike: 'ilike',
+  nlike: 'not like',
+  in: 'in',
+  nin: 'not in',
+}
 
 export const FunctionalOperators: { [T in OperatorName]: (actual: any, expected: any) => boolean } = {
   eq: <T = any>(actual: T, expected: T) => actual === expected,
@@ -21,35 +22,50 @@ export const FunctionalOperators: { [T in OperatorName]: (actual: any, expected:
   le: (actual: number, expected: number) => actual <= expected,
   ge: (actual: number, expected: number) => actual >= expected,
   like: (actual: string, expected: string) => {
-    if (expected.startsWith("%") && expected.endsWith("%")) {
-      return actual.includes(expected.replace(/%/g, ""));
+    if (expected.startsWith('%') && expected.endsWith('%')) {
+      return actual.includes(expected.replace(/%/g, ''))
     }
 
-    if (expected.startsWith("%")) {
-      return actual.endsWith(expected.replace(/%/g, ""));
+    if (expected.startsWith('%')) {
+      return actual.endsWith(expected.replace(/%/g, ''))
     }
 
-    if (expected.endsWith("%")) {
-      return actual.startsWith(expected.replace(/%/g, ""));
+    if (expected.endsWith('%')) {
+      return actual.startsWith(expected.replace(/%/g, ''))
     }
 
-    return false;
+    return false
+  },
+  ilike: (actual: string, expected: string) => {
+    if (expected.startsWith('%') && expected.endsWith('%')) {
+      return actual.toLowerCase().includes(expected.replace(/%/g, '').toLowerCase())
+    }
+
+    if (expected.startsWith('%')) {
+      return actual.toLowerCase().endsWith(expected.replace(/%/g, '').toLowerCase())
+    }
+
+    if (expected.endsWith('%')) {
+      return actual.toLowerCase().startsWith(expected.replace(/%/g, '').toLowerCase())
+    }
+
+    return false
   },
   nlike: (actual: string, expected: string) => {
-    if (expected.startsWith("%") && expected.endsWith("%")) {
-      return !actual.includes(expected.replace(/%/g, ""));
+    if (expected.startsWith('%') && expected.endsWith('%')) {
+      return !actual.includes(expected.replace(/%/g, ''))
     }
 
-    if (expected.startsWith("%")) {
-      return !actual.endsWith(expected.replace(/%/g, ""));
+    if (expected.startsWith('%')) {
+      return !actual.endsWith(expected.replace(/%/g, ''))
     }
 
-    if (expected.endsWith("%")) {
-      return !actual.startsWith(expected.replace(/%/g, ""));
+    if (expected.endsWith('%')) {
+      return !actual.startsWith(expected.replace(/%/g, ''))
     }
 
-    return false;
+    return false
   },
   in: <T = any>(actual: T, expected: T[]) => expected.includes(actual),
   nin: <T = any>(actual: T, expected: T[]) => !expected.includes(actual),
-};
+}
